8c6a8f577ebfa929f74cd7cb389fec5d6a1e8535
Douglas Ramos <doug.ie@outlook.ie>
2023-05-04 15:35:03 -0300

    Fourth LeetCode Exercise

---
+++ 4_Maximum_Subarray/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += 0
+            maxSub = max(maxSub, curSum)
+        return maxSub  
